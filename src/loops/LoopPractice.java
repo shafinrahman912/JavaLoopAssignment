21a99383bba3fab818dc8a3bf33becd695fd6b50
shafinrahman912 <shafinrahman912@outlook.com>
2023-07-04 11:17:01 +0600

    Java Loop Assignment Updated

---
+++ src/loops/LoopPractice.java
@@ -0,0 +1,132 @@
+package loops;
+
+public class LoopPractice {
+
+	/*
+	 * 
+	 * 1
+	 * 
+	 * 2 3
+	 * 
+	 * 4 5 6
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 1
+	 * 
+	 * 2 3
+	 * 
+	 * 4 5 6
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 1 2 3
+	 * 
+	 * 4 5
+	 * 
+	 * 6
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 1 2 3
+	 * 
+	 * 4 5
+	 * 
+	 * 6
+	 * 
+	 * 
+	 * 
+	 * 
+	 *     1
+	 * 
+	 *   2 3 4
+	 * 
+	 * 5 6 7 8 9
+	 */
+
+	// pattern 1
+	public void loopPatternOne() {
+		int counter = 1;
+		for (int i = 1; i <= 3; i++) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print(counter + " ");
+				counter++;
+			}
+			System.out.println();
+		}
+		System.out.println();
+	}
+
+	// pattern 2
+	public void loopPatternTwo() {
+		int counter = 1;
+		int spacePrint = 4;
+		for (int i = 1; i <= 3; i++) {
+			for (int k = 1; k <= spacePrint; k++) {
+				System.out.print(" ");
+			}
+			spacePrint -= 2;
+			for (int j = 1; j <= i; j++) {
+				System.out.print(counter + " ");
+				counter++;
+			}
+			System.out.println();
+		}
+		System.out.println();
+	}
+
+	// pattern 3
+	public void loopPatternThree() {
+		int counter = 1;
+		for (int i = 1; i <= 3; i++) {
+			for (int j = 3; j >= i; j--) {
+				System.out.print(counter + " ");
+				counter++;
+			}
+			System.out.println();
+		}
+		System.out.println();
+	}
+
+	// pattern 4
+	public void loopPatternFour() {
+		int counter = 1;
+		int spacePrint = 0;
+		for (int i = 1; i <= 3; i++) {
+			for (int k = 1; k <= spacePrint; k++) {
+				System.out.print(" ");
+			}
+			spacePrint += 2;
+			for (int j = 3; j >= i; j--) {
+				System.out.print(counter + " ");
+				counter++;
+			}
+			System.out.println();
+		}
+		System.out.println();
+	}
+
+	// pattern 5
+	public void loopPatternFive() {
+		int counter = 1;
+		int spacePrint = 4;
+		int flag = 1;
+		for (int i = 1; i <= 3; i++) {
+			for (int k = 1; k <= spacePrint; k++) {
+				System.out.print(" ");
+			}
+			spacePrint -= 2;
+			for (int j = 1; j <= flag; j++) {
+				System.out.print(counter + " ");
+				counter++;
+			}
+			System.out.println();
+			flag += 2;
+		}
+		System.out.println();
+	}
+
+}
---
+++ src/root/App.java
@@ -0,0 +1,27 @@
+package root;
+
+import loops.LoopPractice;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LoopPractice pattern = new LoopPractice();
+		System.out.println("--------Pattern One--------");
+		pattern.loopPatternOne();
+		
+		System.out.println("--------Pattern Two--------");
+		pattern.loopPatternTwo();
+		
+		System.out.println("--------Pattern Three--------");
+		pattern.loopPatternThree();
+		
+		System.out.println("--------Pattern Four--------");
+		pattern.loopPatternFour();
+		
+		System.out.println("--------Pattern Five--------");
+		pattern.loopPatternFive();
+
+	}
+
+}
